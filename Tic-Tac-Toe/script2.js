55aacdabf62aa268a70f6a41abfd889e7f80b0bb
Calise425 <caitlin.norris1998@gmail.com>
2023-05-04 19:06:12 -0600

    Removed connect-four from tracking, implemented 2 player tic-tac-toe

--- Tic-Tac-Toe/script2.js
+++ Tic-Tac-Toe/script2.js
@@ -1,20 +1,68 @@
-//Allow players to put in names
-//NICE TODO: add line through winning position
+//Variable references!!
 const board = document.getElementById('board');
 const resetButton = document.getElementById('reset');
 const h2 = document.getElementsByTagName('h2')[0];
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
+const playButton = document.getElementById('play');
 
+
+//Variables!! Initial state of things
+//I dont think I have a great grasp on when it's best to use an object. Is this an object angle?
 let cells = [];
 let players = 1;
 let symbol = 'X';
 let computerSymbol = 'O';
 let gameOver = false;
+let p1Name = '';
+let p2Name = '';
+let xPlayer = '';
+let oPlayer = '';
 
+
+//Adding an event listener to the # of players drop down
+//If its 2 players, the name field will pop up
 const select = document.getElementsByTagName('select')[0];
 select.addEventListener('change', (event) => {
-    players = event.target.value;
+    players = parseInt(event.target.value);
     clearBoard();
+    symbol = 'X';
+    const options = document.getElementById('options');
+    if (players === 2){
+        options.style.display = 'flex';
+    } else {
+        options.style.display = 'none';
+    }
 });
+
+//Allows players to input names
+player1.addEventListener('change', (event) => {
+    p1Name = event.target.value;
+});
+player2.addEventListener('change', (event) => {
+    p2Name = event.target.value;
+});
+
+//Random number generator for first player
+function randomNum(min, max) {
+    return parseInt(Math.random() * (max - min) + min);
+};
+
+//Adds eventListener to the play button to randomly roll for which player goes first
+playButton.addEventListener('click', (event) => {
+    const firstPlayer = randomNum(1,3);
+    if (firstPlayer === 1){
+        h2.innerText = `${p1Name}'s move`
+        xPlayer = p1Name;
+        oPlayer = p2Name;
+    } else {
+        h2.innerText = `${p2Name}'s move`
+        xPlayer = p2Name;
+        oPlayer = p1Name;
+    }
+});
+
+
 //Build the cells array
 for (let i = 0; i < 9; i++){
     cells.push("");
@@ -39,8 +87,24 @@ const checkWin = () => {
     for (let i = 0; i < winningPositions.length; i++) {
         const [a, b, c] = winningPositions[i];
         if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
-            h2.innerText = `${cells[a]} is the winner!`;
             gameOver = true;
+            if (cells[a] === 'X'){
+                if (players === 1){
+                    h2.innerText = 'You win!'
+                    gameOver = true;
+                } else {
+                    h2.innerText = `${xPlayer} wins!`
+                    gameOver = true;
+                }
+            } else if (cells[a] === 'O'){
+                if (players === 1){
+                    h2.innerText = 'Computer wins';
+                    gameOver = true;
+                } else {
+                    h2.innerText = `${oPlayer} wins!`
+                    gameOver = true;
+                }
+            }
             return;
         }
     }
@@ -61,9 +125,8 @@ const clearBoard = () => {
     h2.innerText = '';
 };
 
-//Click event listener on the board that checks if the target is a cell, and if the cell textContent is empty
-//If so, it changes the textContent to the current symbol
-//After which if the game isn't over, it will change the player symbol.
+//Adds event listener to the board. In single player, it will also make a move depending on the state of the cells array
+//In two player, it switches whose turn it is
 board.addEventListener('click', function(e){
     if (!gameOver && players === 2){
         if (e.target.className === 'cell'){
@@ -74,14 +137,16 @@ board.addEventListener('click', function(e){
                 if (!gameOver) {
                     if (symbol === "X") {
                         symbol = "O";
+                        h2.innerText = `${oPlayer}'s move`;
                     } else {
                         symbol = "X";
+                        h2.innerText = `${xPlayer}'s move`;
                     }
                 } 
             }     
         }
-    } else if (!gameOver && players === 1){
-        if (e.target.className === 'cell'){
+    } else {
+        if (!gameOver && e.target.className === 'cell'){
             if (!e.target.textContent.length){
                 cells[e.target.id] = symbol;
                 e.target.textContent = symbol;
@@ -109,11 +174,18 @@ board.addEventListener('click', function(e){
                             return;
                         }                                                           
                     }
-                    const blankIndex = cells.indexOf('');
-                    cells[blankIndex] = computerSymbol;
-                    const computerSelection = document.getElementById(`${blankIndex}`)
-                    computerSelection.textContent = computerSymbol;
-                    checkWin();
+                    if (!cells[4]){
+                        cells[4] = computerSymbol;
+                        const computerSelection = document.getElementById(`${4}`)
+                        computerSelection.textContent = computerSymbol;
+                        checkWin();
+                    } else {
+                        const blankIndex = cells.indexOf('');
+                        cells[blankIndex] = computerSymbol;
+                        const computerSelection = document.getElementById(`${blankIndex}`)
+                        computerSelection.textContent = computerSymbol;
+                        checkWin();
+                    } 
                 }
             }
         }
@@ -123,5 +195,17 @@ board.addEventListener('click', function(e){
 //Add event listener to the reset button to run the clearBoard function
 resetButton.addEventListener('click', () => {
     clearBoard();
+    if (players === 2){
+        const firstPlayer = randomNum(1,3);
+        if (firstPlayer === 1){
+            h2.innerText = `${p1Name}'s move`
+            xPlayer = p1Name;
+            oPlayer = p2Name;
+        } else {
+            h2.innerText = `${p2Name}'s move`
+            xPlayer = p2Name;
+            oPlayer = p1Name;
+        }
+    }
 });
 
